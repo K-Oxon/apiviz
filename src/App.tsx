4e74143ba8fbf04fd31e8090e022b493906ed120
K-Oxon <ko1011qfp@gmail.com>
2024-07-06 18:36:11 +0900

    init pj setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+    return (
+    <div className="App">
+        <h1 className="text-3xl font-bold underline">
+        SPA Data Explorer
+        </h1>
+    </div>
+    )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [react()],
+    build: {
+    outDir: 'dist',
+    sourcemap: true,
+    },
+    server: {
+    port: 3000,
+    },
+});
